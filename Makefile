e5ddf4bb969767c3dbe18ded40f7fe1220ee3836
raspberrypi <luke@dalfonso.net>
2018-04-21 16:45:28 +0000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -o ripper ripper.cpp -lwiringPi
---
+++ ripper.cpp
@@ -0,0 +1,13 @@
+#include<wiringPi.h>
+
+using namespace std;
+
+int main(void){
+	wiringPiSetupGpio();
+	// GPIO pin setup code goes here
+
+	for(;;){
+	// Ripper loop goes here
+
+	}
+}
